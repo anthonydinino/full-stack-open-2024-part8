ec2b86f8fa9e695c0b6ceb3e686e636d66ddfccb
anthonydinino <adinino1996@gmail.com>
2024-02-04 23:05:02 +1030

    Exercise 8.9 complete

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,5 +1,9 @@
+import { useQuery } from "@apollo/client";
+import { ALL_BOOKS } from "../queries";
+
 const Books = () => {
-  const books = [];
+  const result = useQuery(ALL_BOOKS);
+  const books = result.loading ? [] : result.data.allBooks;
 
   return (
     <div>
